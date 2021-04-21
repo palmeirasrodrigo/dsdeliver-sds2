8fa6885f82d344055e2dd5bd738e71a748b7fab8
Rodrigo Negrão de Jesus <palmeirasrodrigo@hotmail.com>
2021-04-21 20:54:50 -0300

    put/orders

--- backend/src/main/java/com/devsuperior/dsdeliver/resources/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/resources/OrderController.java
@@ -18,15 +18,21 @@ public class OrderController {
     private OrderService service;
 
     @GetMapping
-    public ResponseEntity<List<OrderDTO>>findAll(){
-        List<OrderDTO>list = service.findAll();
+    public ResponseEntity<List<OrderDTO>> findAll() {
+        List<OrderDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @PostMapping
-    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
