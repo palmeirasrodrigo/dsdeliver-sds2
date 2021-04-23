ed4066dcf0871dfd6d812d0b0c491a8f225a639f
Rodrigo Negrão de Jesus <palmeirasrodrigo@hotmail.com>
2021-04-23 16:00:20 -0300

    Component Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   return (
-    <div >
-     <h1>Hello</h1>
-    </div>
+    <>
+     <Navbar/>
+     <Home/>
+    </>
   );
 }
 
