c090e81ab0453af2884428409ccd4280db96eb35
Rodrigo Negrão de Jesus <palmeirasrodrigo@hotmail.com>
2021-04-23 19:58:47 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,29 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      App desenvolvido no evento Semana DevSuperior
+      <div className="footer-icons">
+        <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+          <YouTubeIcon />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/rodrigo-negr%C3%A3o-de-jesus-3b0758156/"
+          target="_new"
+        >
+          <LinkedinIcon />
+        </a>
+        <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+          <InstagramIcon />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
