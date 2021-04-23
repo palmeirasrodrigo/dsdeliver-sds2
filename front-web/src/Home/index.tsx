31c94e6a9c4856ebf3bbd4a26949d1f0643a13c8
Rodrigo Negrão de Jesus <palmeirasrodrigo@hotmail.com>
2021-04-23 18:48:22 -0300

    Add Home Page

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,3 +1,6 @@
+import './style.css'
+import {ReactComponent as MainImage} from './main.svg'
+
 function Home() {
     return (
       <div className="home-container">
@@ -5,7 +8,9 @@ function Home() {
               <div className="home-actions">
                   <h1 className="home-title">Faça seu pedido <br/>que entregamos <br/> pra você</h1>
                   <h3 className="home-subtitle">Escolha o seu pedido e em poucos minutos <br/> levaremos na sua porta</h3>
+                  <a href="rotas" className="home-btn-order">FAZER PEDIDO</a>
               </div>
+              <div className="home-image"><MainImage/></div>
           </div>
       </div>
     )
