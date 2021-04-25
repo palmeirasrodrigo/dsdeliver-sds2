05bd5f24711e87d4d4840ac6d48e62171256fa1a
Rodrigo Negrão de Jesus <palmeirasrodrigo@hotmail.com>
2021-04-25 14:56:57 -0300

    Map

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,31 @@
 import './styles.css';
 import StepHeader from './StepHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { OrderLocationData, Product } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="ORDERS-CONTAINER">
+    <>
+    <div className="orders-container">
       <StepHeader />
+      <ProductsList products={products} />
+      <OrderLocation
+        onChangeLocation={(location) => setOrderLocation(location)} //evento do filho para o pai
+      />
     </div>
+    </>
   );
 }
 
